3d47fa87774ce4c4059013a3b3baede804a44fcb
Erik Mattfolk <erik.mattfolk@gmail.com>
2022-07-10 17:58:21 +0200

    Do some simple parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Main where
 
-import Lib
+import Lib (someFunc)
+import Std
 
 main :: IO ()
 main = someFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,46 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Lib (someFunc) where
 
+import Std
+import Text.Parsec (alphaNum, lower, many, runParser, spaces)
+import Text.Parsec.String (Parser)
+
+-- AST = statement...
+-- statement = definition | ...
+-- definition = identifier identifier... '=' value
+-- value = int | call
+-- call = identifier identifier...
+-- identifier = [a-z][a-zA-Z0-9]+
+-- int = [1-9][0-9]+
+
+newtype Identifier = Identifier String
+  deriving (Show)
+
+type AST = [Statement]
+
+data Statement
+  = Definition Identifier [Identifier]
+  deriving (Show)
+
+parseIdentifier :: Parser Identifier
+parseIdentifier =
+  do
+    firstLetter <- lower
+    rest <- many alphaNum
+    return $ Identifier (firstLetter : rest)
+
+parseStatement :: Parser Statement
+parseStatement =
+  do
+    name <- parseIdentifier
+    args <- many (spaces *> parseIdentifier)
+    return $ Definition name args
+
+parse :: Parser AST
+parse = many parseStatement
+
 someFunc :: IO ()
-someFunc = putStrLn "Hello world!"
+someFunc =
+  let res = runParser parse () "Parsing variable" "hello123 arg1 arg2"
+   in print res
